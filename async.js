1021a07ba78a86816087eef5a103bc0727ba2dd7
Gusarov Vadim <gusarovvadim1996@gmail.com>
2017-11-29 23:47:48 +0500

    done

--- async.js
+++ async.js
@@ -3,11 +3,37 @@
 exports.isStar = true;
 exports.runParallel = runParallel;
 
-/** Функция паралелльно запускает указанное число промисов
- * @param {Array} jobs – функции, которые возвращают промисы
- * @param {Number} parallelNum - число одновременно исполняющихся промисов
- * @param {Number} timeout - таймаут работы промиса
- */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise(resolve => {
+        let numberOfJob = 0;
+        let results = [];
+        if (jobs.length === 0) {
+            resolve(results);
+        }
+        for (let count = 0; count < parallelNum; count++) {
+            startWorking(jobs[numberOfJob], numberOfJob);
+            numberOfJob += 1;
+        }
+
+        function startWorking(job, index) {
+            let finish = result => finishWorking(result, index);
+            new Promise((resolveOfJob, rejectOfJob) => {
+                job().then(resolveOfJob, rejectOfJob);
+                setTimeout(rejectOfJob, timeout, new Error('Promise timeout'));
+            }).then(finish, finish);
+        }
+
+        function finishWorking(result, index) {
+            results[index] = result;
+            if (results.length === jobs.length) {
+                resolve(results);
+
+                return true;
+            }
+            if (numberOfJob < jobs.length) {
+                startWorking(jobs[numberOfJob], numberOfJob);
+                numberOfJob += 1;
+            }
+        }
+    });
 }
